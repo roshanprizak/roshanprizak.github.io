a93ee2732a8de19541c98262ff8581656765029a
roshanprizak <roshanprizak@gmail.com>
2022-08-31 19:24:45 +0200

    all website update

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,3 +7,32 @@ $(document).ready(function() {
     });
     $('a').removeClass('waves-effect waves-light');
 });
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, projects, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    projects = document.getElementsByClassName("projects");
+    for (i = 0; i < projects.length; i++) {
+      projects[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tabcontent" and hide them
+    projects = document.getElementsByClassName("projects_all");
+    for (i = 0; i < projects.length; i++) {
+      projects[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "inherit";
+    document.getElementById(cityName).style.visibility = "visible"
+    evt.currentTarget.className += " active";
+    
+}
