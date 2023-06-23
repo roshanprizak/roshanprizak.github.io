42270d3f09ab136af2c6bebd5a2442836b177795
roshanprizak <roshanprizak@gmail.com>
2023-06-23 19:01:49 +0200

    hero heroine grid, minor corrections

---
+++ projects/the_india_project/movies_india/heroes.js
@@ -0,0 +1,20 @@
+
+// Generate the JavaScript array dynamically
+var actors = ["N. T. Rama Rao", "Akkineni Nageswara Rao", "Krishna", "Rajendra Prasad", "Chiranjeevi", "Sobhan Babu", "Nandamuri Balakrishna", "Jagapathi Babu", "Nagarjuna", "Daggubati Venkatesh", "Krishnam Raju", "Srikanth", "Mohan Babu", "Kanta Rao", "Rajasekhar", "Ravi Teja", "Allari Naresh", "Chandra Mohan", "Suman", "Jaggayya", "Naresh", "Sivaji", "Jr. NTR", "Gummadi", "Mahesh Babu", "Srihari", "Nithiin", "Pawan Kalyan", "Sumanth", "Nani", "Gopichand", "Murali Mohan", "Kamal Haasan", "Prabhas", "J. D. Chakravarthy", "Sharwanand", "Varun Sandesh", "Tarun", "Allu Arjun", "Vishnu Manchu", "Kalyan Ram", "Naga Chaitanya", "Suresh", "Navdeep", "Naga Shourya", "S. V. Ranga Rao", "Vinod Kumar", "Nara Rohit", "Sundeep Kishan", "V. Nagayya", "Aadi", "Ali", "Uday Kiran", "Sunil", "Nikhil Siddharth", "R. Narayana Murthy", "Manoj Manchu", "Raj Tarun", "Sai Dharam Tej", "K. Raghuramaiah", "Rajinikanth", "Sai Kumar", "Vadde Naveen", "Venu", "Ram Charan", "C. H. Narayana Rao", "Bhanuchander", "Siddharth", "Tanish"];
+
+// Generate the HTML code dynamically
+var html = "";
+for (var i = 0; i < actors.length; i++) {
+    var actor = actors[i];
+    var avatarImagePath = `avatars/${actor}.jpg`;
+    var figureImagePath = `hero_plots/${actor}.png`;
+    
+    html += `
+    <a href="${figureImagePath}" class="lightbox hero-link" title="${actor}" data-gallery="my-gallery">
+        <img src="${avatarImagePath}" width="75px" height="75px" class="avatar">
+    </a>
+    `;
+}
+
+// Insert the generated HTML code into the desired element on your web page
+document.getElementById("heroes-section").innerHTML = html;
---
+++ projects/the_india_project/movies_india/heroines.js
@@ -0,0 +1,20 @@
+
+// Generate the JavaScript array dynamically
+var actors = ["Jayasudha", "Jamuna", "Vijayashanti", "Savitri", "Vanisri", "Anjali Devi", "Sridevi", "Krishna Kumari", "Jaya Prada", "Soundarya", "Ramya Krishna", "Meena", "Sharada", "Radha", "P. Bhanumathi", "Bhanupriya", "Suhasini", "Kajal Aggarwal", "Vijaya Nirmala", "Raasi", "Charmy Kaur", "Raadhika", "Sowcar Janaki", "Anushka Shetty", "Tamannaah", "B. Saroja Devi", "Shobana", "Roja", "Shriya Saran", "Kanchana", "Samantha", "G. Varalakshmi", "Manjula", "Madhavi", "Rajani", "Lakshmi", "Devika", "Jayalalithaa", "Trisha Krishnan", "Simran", "Bhoomika Chawla", "Rakul Preet Singh", "Rambha", "Priyamani", "Rajasulochana", "Nayantara", "Raashi Khanna", "S. Varalakshmi", "Sujatha", "Nagma", "Lavanya Tripathi", "C. Krishnaveni", "C. Lakshmi Rajyam", "Sriranjani Jr.", "Jayachitra", "Sumalatha", "Kalyani", "Sanghavi", "Laya", "Ileana D'Cruz", "Regina Cassandra", "Gautami", "Aamani", "Genelia D'Souza", "Shruti Haasan", "Chandrakala", "Vani Viswanath", "Sneha", "Amala"];
+
+// Generate the HTML code dynamically
+var html = "";
+for (var i = 0; i < actors.length; i++) {
+    var actor = actors[i];
+    var avatarImagePath = `avatars/${actor}.jpg`;
+    var figureImagePath = `heroine_plots/${actor}.png`;
+    
+    html += `
+    <a href="${figureImagePath}" class="lightbox hero-link" title="${actor}" data-gallery="my-gallery">
+        <img src="${avatarImagePath}" width="75px" height="75px" class="avatar">
+    </a>
+    `;
+}
+
+// Insert the generated HTML code into the desired element on your web page
+document.getElementById("heroines-section").innerHTML = html;
